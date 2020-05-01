af1c1e2c42e60a85125200805260a41cef05d5a3
jared <38332939+jaredaritter@users.noreply.github.com>
2020-04-30 20:26:06 -0400

    Add reset button. Update names for clarity.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const reset = document.querySelector('#reset');
 const container = document.querySelector('#container');
+
+function changeColor() {
+    this.classList.add('painted');
+}
+
+function resetGrid() {
+// BUTTON CLEARS CURRENT GRID
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.classList.remove('painted'));
+// BUTTON PROMPTS USER FOR SIDE LENGTH FOR NEW GRID
+    let sideLength = Number(prompt('How long should each side be?'));
+    while (sideLength === NaN) {
+        sideLength = Number(prompt('How long should each side be?'));
+    }
+    console.log(sideLength);
+// 960PX WIDTH
+}
 
 for (let i = 0; i < 64; i++) {
     const div = document.createElement('div');
@@ -7,6 +25,4 @@ for (let i = 0; i < 64; i++) {
     container.appendChild(div);
 }
 
-function changeColor() {
-    this.style.backgroundColor = 'red';
-}
+reset.addEventListener('click', resetGrid);
