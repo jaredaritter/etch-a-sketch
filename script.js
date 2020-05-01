cf20db59e73d62583807e3a5fc56011f65892b67
jared <38332939+jaredaritter@users.noreply.github.com>
2020-05-01 15:09:47 -0400

    Dynamic  sizing for grid applied and functional.

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-const reset = document.querySelector('#reset');
+const newGrid = document.querySelector('#newGrid');
 const container = document.querySelector('#container');
 
 function changeColor() {
     this.classList.add('painted');
 }
 
-function resetGrid() {
-// BUTTON CLEARS CURRENT GRID
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.classList.remove('painted'));
+function makeGrid() {
+    // REMOVE OLD GRID
+    const container = document.querySelector('#container')
+    container.textContent = '';
 // BUTTON PROMPTS USER FOR SIDE LENGTH FOR NEW GRID
     let sideLength = Number(prompt('How long should each side be?'));
-    while (sideLength === NaN) {
+    while (isNaN(sideLength)) {
         sideLength = Number(prompt('How long should each side be?'));
     }
-    console.log(sideLength);
-// 960PX WIDTH
+    container.setAttribute('style', 'grid-template-columns: repeat('+ sideLength +', 1fr)');
+// 960PX MAX WIDTH
+    for (let i = 0; i < sideLength ** 2; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.addEventListener('mouseover', changeColor);
+        container.appendChild(div);
+}
 }
 
-for (let i = 0; i < 64; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    div.addEventListener('mouseover', changeColor);
-    container.appendChild(div);
-}
-
-reset.addEventListener('click', resetGrid);
+newGrid.addEventListener('click', makeGrid);
