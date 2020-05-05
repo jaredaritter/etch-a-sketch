e2f3730b91be21f0283bc90d52406fa9e97b10ce
jared <38332939+jaredaritter@users.noreply.github.com>
2020-05-05 11:57:45 -0400

    Add random button and functionality. Currently will not toggle or overwrite due to priority of inline styling over class

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const newGrid = document.querySelector('#newGrid');
 const silver = document.querySelector('#silver');
+const random = document.querySelector('#random');
 const lighten = document.querySelector('#lighten');
+
 let colorChoice = 'default';
 let opacity = 1;
 
@@ -13,10 +15,14 @@ function changeColor() {
         this.classList.remove('lighten');
         this.classList.remove('painted');
         this.classList.add('silver');
-    } else {
+    } else if (colorChoice === 'lighten') {
         // this.classList.remove('painted');
         // this.classList.remove('silver');
         this.classList.add('lighten');
+    } else if (colorChoice === 'random') {
+        this.classList.remove('lighten');
+        this.classList.remove('painted');
+        this.style.backgroundColor = randomColor();
     }
 }
 
@@ -43,9 +49,10 @@ function makeGrid() {
     for (let i = 0; i < sideLength ** 2; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
-        div.addEventListener('mouseover', changeColor);
         container.appendChild(div);
     }
+    // SETS MOUSEOVER LISTENERS
+    document.querySelectorAll('.square').forEach(square => square.addEventListener('mouseover', changeColor));
 }
 
 function toggleSilver(e) {
@@ -68,9 +75,39 @@ function toggleLighten(e) {
     }
 }
 
+function randomNumber(max) {
+    let random = Math.floor(Math.random() * (max + 1));
+    return random;
+}
+
+function randomColor() {
+    const R = randomNumber(255);
+    const G = randomNumber(255);
+    const B = randomNumber(255);
+    return `rgb(${R}, ${G}, ${B})`;
+}
+
 newGrid.addEventListener('click', makeGrid);
 silver.addEventListener('click', toggleSilver);
 lighten.addEventListener('click', toggleLighten);
+random.addEventListener('click', () => {
+    colorChoice = 'random';
+});
 
 
 // functionality to input color choice?
+
+// ASSESS FUNCTIONALITY OF RANDOM NUMBER GENERATOR AT 0 AND 255
+function checkHighLow(num) {
+    let low = 0;
+    let high = 0;
+    for (let i = 0; i < num; i++) {
+        let guess = randomNumber(255);
+        if (guess === 0) {
+            low++;
+        } else if (guess === 255) {
+            high++;
+        }
+    }
+    console.log(`Low: ${low} High: ${high}`);
+}
